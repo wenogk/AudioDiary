d13452c77f714ac177440ab36ea667449009b84f
wenogk <drf325@nyu.edu>
2020-07-30 18:53:08 +0400

    styling for positive line chart started

--- Audio Diary/View Controllers/StatsViewController.swift
+++ Audio Diary/View Controllers/StatsViewController.swift
@@ -12,16 +12,20 @@ import CoreData
 
 class StatsViewController: UIViewController, ChartViewDelegate {
     
+    @IBOutlet var stackView: UIStackView!
+    
+    
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
     
-    var lineChart = LineChartView();
+    var positivityLineChart = LineChartView();
     
     var audioItems:[AudioItem]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        lineChart.delegate = self;
+        positivityLineChart.delegate = self;
         // Do any additional setup after loading the view.
+        //positivityLineChart.backgroundColor = UIColor(red: 104/255, green: 241/255, blue: 175/255, alpha: 1)
         
     }
     
@@ -42,8 +46,8 @@ class StatsViewController: UIViewController, ChartViewDelegate {
             
             DispatchQueue.main.async {
                 //self.tableView.reloadData();
-                self.loadBarChartData()
-                self.lineChart.notifyDataSetChanged(); // let the chart know it's data changed
+                self.loadPositiveBarChartData()
+                self.positivityLineChart.notifyDataSetChanged(); // let the chart know it's data changed
                // self.barChart.invalidate(); // refresh
             }
             
@@ -54,7 +58,10 @@ class StatsViewController: UIViewController, ChartViewDelegate {
         
     }
     
-    func loadBarChartData() {
+    func loadPositiveBarChartData() {
+        
+        //transform data from audioItem array and create datapoints for positive bar chart
+        
         var entries = [ChartDataEntry]()
         
         guard self.audioItems != nil else { return; }
@@ -63,19 +70,27 @@ class StatsViewController: UIViewController, ChartViewDelegate {
             entries.append(ChartDataEntry(x: Double(x), y: self.audioItems![x-1].positiveProbability))
         }
         
-        let set = LineChartDataSet(entries: entries);
+        let set = LineChartDataSet(entries: entries, label: "Positive datapoints");
+        set.mode = .cubicBezier
+        set.drawCirclesEnabled = false
+        set.lineWidth = 1.8
+        set.circleRadius = 4
+        set.setCircleColor(.white)
+        set.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set.fillColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+        set.fillAlpha = 1
+        set.drawFilledEnabled = true;
+        
+       // set.drawHorizontalHighlightIndicatorEnabled = false
         let data = LineChartData(dataSet: set)
-        lineChart.data = data;
+        positivityLineChart.data = data;
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews();
         
-        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
-        
-        lineChart.center = view.center
-        
-        view.addSubview(lineChart)
+        positivityLineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
+        stackView.addSubview(positivityLineChart)
         
         
     }
