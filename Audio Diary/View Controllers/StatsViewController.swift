5db6f5b972b456169ffc5ddcbc8be9dc9850a58a
wenogk <drf325@nyu.edu>
2020-07-30 18:15:31 +0400

    added Charts library from cocoa pods and implemented a sample line chart that takes audio item data from core data and plots it!

--- Audio Diary/View Controllers/StatsViewController.swift
+++ Audio Diary/View Controllers/StatsViewController.swift
@@ -7,15 +7,78 @@
 //
 
 import UIKit
+import Charts
+import CoreData
 
-class StatsViewController: UIViewController {
-
+class StatsViewController: UIViewController, ChartViewDelegate {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
+    
+    var lineChart = LineChartView();
+    
+    var audioItems:[AudioItem]?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        lineChart.delegate = self;
         // Do any additional setup after loading the view.
+        
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        fetchAudioItems()
+    }
+    
+    func fetchAudioItems() {
+        
+        do {
+            let request = AudioItem.fetchRequest() as NSFetchRequest<AudioItem>
+            
+            let sort = NSSortDescriptor(key: "dateTime", ascending: true)
+            
+            request.sortDescriptors = [sort]
+            
+            self.audioItems = try context.fetch(request)
+            
+            DispatchQueue.main.async {
+                //self.tableView.reloadData();
+                self.loadBarChartData()
+                self.lineChart.notifyDataSetChanged(); // let the chart know it's data changed
+               // self.barChart.invalidate(); // refresh
+            }
+            
+        } catch {
+            
+        }
+        
+        
+    }
+    
+    func loadBarChartData() {
+        var entries = [ChartDataEntry]()
+        
+        guard self.audioItems != nil else { return; }
+        
+        for x in 1...self.audioItems!.count {
+            entries.append(ChartDataEntry(x: Double(x), y: self.audioItems![x-1].positiveProbability))
+        }
+        
+        let set = LineChartDataSet(entries: entries);
+        let data = LineChartData(dataSet: set)
+        lineChart.data = data;
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews();
+        
+        lineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
+        
+        lineChart.center = view.center
+        
+        view.addSubview(lineChart)
+        
+        
+    }
 
     /*
     // MARK: - Navigation
