c21fc9ae7d01995d51857fa4b873c7bd67fbd7a5
wenogk <drf325@nyu.edu>
2020-07-31 10:21:59 +0400

    two line charts, still not scrollable view tho

--- Audio Diary/View Controllers/StatsViewController.swift
+++ Audio Diary/View Controllers/StatsViewController.swift
@@ -18,12 +18,14 @@ class StatsViewController: UIViewController, ChartViewDelegate {
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
     
     var positivityLineChart = LineChartView();
+    var negativityLineChart = LineChartView();
     
     var audioItems:[AudioItem]?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         positivityLineChart.delegate = self;
+        negativityLineChart.delegate = self;
         // Do any additional setup after loading the view.
         //positivityLineChart.backgroundColor = UIColor(red: 104/255, green: 241/255, blue: 175/255, alpha: 1)
         
@@ -47,7 +49,9 @@ class StatsViewController: UIViewController, ChartViewDelegate {
             DispatchQueue.main.async {
                 //self.tableView.reloadData();
                 self.loadPositiveBarChartData()
+                self.loadNegativeBarChartData()
                 self.positivityLineChart.notifyDataSetChanged(); // let the chart know it's data changed
+                self.negativityLineChart.notifyDataSetChanged();
                // self.barChart.invalidate(); // refresh
             }
             
@@ -56,6 +60,21 @@ class StatsViewController: UIViewController, ChartViewDelegate {
         }
         
         
+    }
+    
+    func generateStyledDataSetForLineChart(entries: [ChartDataEntry], label: String, fillColor: UIColor) -> LineChartDataSet {
+        let set = LineChartDataSet(entries: entries, label: label);
+        set.mode = .cubicBezier
+        set.drawCirclesEnabled = false
+        set.lineWidth = 1.8
+        set.circleRadius = 4
+        set.setCircleColor(.white)
+        set.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
+        set.fillColor = fillColor
+        set.fillAlpha = 1
+        set.drawFilledEnabled = true;
+        
+        return set;
     }
     
     func loadPositiveBarChartData() {
@@ -70,27 +89,42 @@ class StatsViewController: UIViewController, ChartViewDelegate {
             entries.append(ChartDataEntry(x: Double(x), y: self.audioItems![x-1].positiveProbability))
         }
         
-        let set = LineChartDataSet(entries: entries, label: "Positive datapoints");
-        set.mode = .cubicBezier
-        set.drawCirclesEnabled = false
-        set.lineWidth = 1.8
-        set.circleRadius = 4
-        set.setCircleColor(.white)
-        set.highlightColor = UIColor(red: 244/255, green: 117/255, blue: 117/255, alpha: 1)
-        set.fillColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-        set.fillAlpha = 1
-        set.drawFilledEnabled = true;
+        let set = generateStyledDataSetForLineChart(entries: entries, label: "positive",fillColor: UIColor(red: 0, green: 1, blue: 0, alpha: 0.5))
         
        // set.drawHorizontalHighlightIndicatorEnabled = false
         let data = LineChartData(dataSet: set)
         positivityLineChart.data = data;
     }
     
+    func loadNegativeBarChartData() {
+        
+        //transform data from audioItem array and create datapoints for positive bar chart
+        
+        var entries = [ChartDataEntry]()
+        
+        guard self.audioItems != nil else { return; }
+        
+        for x in 1...self.audioItems!.count {
+            entries.append(ChartDataEntry(x: Double(x), y: self.audioItems![x-1].negativeProbability))
+        }
+        
+        let set = generateStyledDataSetForLineChart(entries: entries, label: "negative", fillColor: UIColor(red: 1, green: 0, blue: 0, alpha: 0.5))
+        
+       // set.drawHorizontalHighlightIndicatorEnabled = false
+        let data = LineChartData(dataSet: set)
+        negativityLineChart.data = data;
+    }
+    
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews();
         
         positivityLineChart.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.width)
+        
+        negativityLineChart.frame = CGRect(x: 0, y: self.view.frame.width, width: self.view.frame.width, height: self.view.frame.width)
+        
+        
         stackView.addSubview(positivityLineChart)
+        stackView.addSubview(negativityLineChart)
         
         
     }
